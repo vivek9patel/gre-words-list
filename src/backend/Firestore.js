31e8c6eaa36431c5c64502323bcbf5e69b8364b0
vivek9patel <vivek.p9737@gmail.com>
2021-08-22 09:27:50 +0530

    init firebase

---
+++ src/backend/Firestore.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "gre-words-list.firebaseapp.com",
+    projectId: "gre-words-list",
+    storageBucket: "gre-words-list.appspot.com",
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+firebase.initializeApp(config);
+export default firebase;
