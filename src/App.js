0eb51d913724470d4aaa952c95005925204279d4
vivek9patel <vivek.p9737@gmail.com>
2021-08-23 22:45:30 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddWord from "./components/AddWord";
 
 function App() {
   return (
-    <Box bg="cyan.500" w="100vw" h="100vh">
+    <Box mb="10" bg="cyan.500" w="100vw">
       <Header />
       <Flex mt="4" px="4" py="2" align="flex-end" justify="flex-end">
         <AddWord />
