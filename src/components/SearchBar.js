e74a0837fd27203784dfed254a786e0faa0398ef
vivek9patel <vivek.p9737@gmail.com>
2021-08-23 10:32:46 +0530

    feat(keyEvent): adds '/' keydown to focus search element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { InputGroup, InputLeftElement, Input, InputRightElement, Button } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 
 function SearchBar() {
+
+    useEffect(() => {
+        window.addEventListener('keydown', focusSearchBar);
+        return () => {
+            window.removeEventListener('keydown', focusSearchBar);
+        }
+    }, [])
+
+    const focusSearchBar = (e) => {
+        const searchInput = document.getElementById('search-input');
+
+        if (searchInput === document.activeElement) {
+            return;
+        }
+
+        if (e.key === '/') {
+            e.preventDefault();
+            searchInput.focus();
+            document.execCommand("selectall", null, false);
+        }
+    }
+
     return (
         <InputGroup color="white">
             <InputLeftElement
                 pointerEvents="none"
                 children={<SearchIcon color="teal" />}
             />
-            <Input bg="white" shadow="md" color="teal" focusBorderColor="teal.500" type="text" placeholder="Search Word" />
+            <Input id="search-input" bg="white" shadow="md" color="teal" focusBorderColor="teal.500" type="text" placeholder="Search Word" />
             <InputRightElement >
                 <Button color="teal.500" fontSize="lg" size="sm" variant="unstyled" borderRadius="md" fontWeight="semibold">
                     /
